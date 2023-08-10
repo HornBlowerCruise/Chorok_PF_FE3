bf0b3823cb32d516a6a71dad57c81117f82a67cd
HornBlowerCruise <immanuel0688@gmail.com>
2023-08-10 17:46:15 +0900

    first commit

--- src/Shared/OAuthKaKao.js
+++ src/Shared/OAuthKaKao.js
@@ -1,5 +1,5 @@
-;
+
 const REACT_APP_KAKAO_REDIRECT_URI =  "http://localhost:3000/auth/kakao/callback"; // local
 // const REACT_APP_KAKAO_REDIRECT_URI =  "https://chorok.shop/auth/kakao/callback"; // deploy
-// export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=61987d99dfe1738afbdd8c691b70409a&redirect_uri=${REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
+// export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=61987d99dfe1738afbdd8c691b70409a&redirect_uri=${REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
