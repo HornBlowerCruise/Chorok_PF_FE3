c5dbd29b9e717dd2410cc15095978f060ba1757b
HornBlowerCruise <immanuel0688@gmail.com>
2023-08-11 10:21:11 +0900

    chg domain

--- src/Shared/OAuthKaKao.js
+++ src/Shared/OAuthKaKao.js
@@ -1,5 +1,5 @@
 
-const REACT_APP_KAKAO_REDIRECT_URI =  "http://localhost:3000/auth/kakao/callback"; // local
-// const REACT_APP_KAKAO_REDIRECT_URI =  "https://chorok.shop/auth/kakao/callback"; // deploy
+// const REACT_APP_KAKAO_REDIRECT_URI =  "http://localhost:3000/auth/kakao/callback"; // local
+const REACT_APP_KAKAO_REDIRECT_URI =  "https://chorok.netlify.app/auth/kakao/callback"; // deploy
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
 //
